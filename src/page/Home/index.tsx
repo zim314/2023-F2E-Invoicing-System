4a7f212ee86dde2dba3517ca901de14fc177bb72
hongzhu zhao <hongzhu.zhao@kangdainfo.com>
2023-12-27 14:09:34 +0800

    feat: 🎸 新增home頁面button

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -2,8 +2,13 @@ import './index.scss';
 import Footer from '../../component/Footer';
 import logo from '../../assets/images/logo.svg';
 import CandidateCard from '../../component/CandidateCard';
-import chuYuFullBody from '../../assets/images/chuYu_fullBody.png';
-import chuYuNumberImg from '../../assets/images/chuYu_numberImage.png';
+import chuYuPicture from '../../assets/images/chuYu_fullBody.png';
+import chuYuNumber from '../../assets/images/chuYu_number.png';
+import yingWenPicture from '../../assets/images/yingWen_fullBody.png';
+import yingWenNumber from '../../assets/images/yingWen_number.png';
+import guoYuPicture from '../../assets/images/guoYu_fullBody.png';
+import guoYuNumber from '../../assets/images/guoYu_number.png';
+import taiwanIcon from '../../assets/images/button_taiwanIcon.svg';
 
 const Home = () => {
     return (
@@ -13,13 +18,24 @@ const Home = () => {
                     <img src={logo} alt="LOGO" />
                     <p>總統大選開票</p>
                 </div>
-                <div>
+                <div className="home__content">
                     <CandidateCard
-                        condidatePicture={chuYuFullBody}
-                        condidateNumber={chuYuNumberImg}
+                        condidatePicture={chuYuPicture}
+                        condidateNumber={chuYuNumber}
+                    />
+                    <CandidateCard
+                        condidatePicture={yingWenPicture}
+                        condidateNumber={yingWenNumber}
+                    />
+                    <CandidateCard
+                        condidatePicture={guoYuPicture}
+                        condidateNumber={guoYuNumber}
                     />
                 </div>
-                <button>開票地圖</button>
+                <button className="alingCenter home__button">
+                    <img src={taiwanIcon} alt="icon" />
+                    <p>開票地圖 ➞</p>
+                </button>
             </div>
             <Footer />
         </div>

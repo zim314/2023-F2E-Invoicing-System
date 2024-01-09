dcc6a44b89aa8751b7e823dd38170baaefb8cd07
hongzhu zhao <hongzhu.zhao@kangdainfo.com>
2024-01-09 14:16:38 +0800

    feat: 🎸 新增CandidateInfo頁面 手機版完成

--- src/page/CandidateInfo/candidateData.ts
+++ src/page/CandidateInfo/candidateData.ts
@@ -1,3 +1,101 @@
-const xxx = {};
+import chuYu_fullBody from '../../assets/images/chuYu_fullBody.png';
+import yingWen_fullBody from '../../assets/images/yingWen_fullBody.png';
+import guoYu_fullBody from '../../assets/images/guoYu_fullBody.png';
+import blueIcon from '../../assets/images/blueIcon.png';
+import greenIcon from '../../assets/images/greenIcon.png';
+import orangeIcon from '../../assets/images/orangeIcon.png';
 
-export default xxx;
+import chuYu_headShot from '../../assets/images/chuYu_headShot.png';
+import yingWen_headShot from '../../assets/images/yingWen_headShot.png';
+import guoYu_headShot from '../../assets/images/guoYu_headShot.png';
+
+const candidateData = [
+    {
+        candidate: 'chuYu',
+        chineseName: '喵楚魚',
+        number: '1',
+        fullBodyPhoto: chuYu_fullBody,
+        headShot: chuYu_headShot,
+        partisan: '橘喵黨',
+        partisanIcon: orangeIcon,
+        partner: '魚喵喵(無黨籍)',
+        background: '政治界：公職、國際關西研究員',
+        learningPortfolio: [
+            '國立政治大學外交系畢業（1964）',
+            '美國公教大學圖書管理學碩士（1971）',
+            '美國喬治城大學政治學博士（1974）',
+        ],
+        experience: [
+            '行政院簡任秘書（1974-1977）',
+            '行政院新聞局副局長（1977-1979）',
+            '總統府簡任秘書（1978-1981、1984-1989）',
+            '行政院新聞局局長（1979-1984）',
+            '中國國民黨文工會主任（1984-1987）',
+            '中國國民黨副秘書長（1987-1989）',
+            '中國國民黨秘書長（1989-1993）',
+            '臺灣省主席（1993-1994）',
+            '臺灣省省長（1994-1998）',
+            '親民黨主席（2000-）',
+            '臺北市政府首席顧問（2014-）',
+            '亞太經合會中華臺北領袖代表（2016-2017） ',
+            '總統府資政（2016-2019）',
+        ],
+    },
+    {
+        candidate: 'yingWen',
+        chineseName: '喵英文',
+        number: '2',
+        fullBodyPhoto: yingWen_fullBody,
+        headShot: yingWen_headShot,
+        partisan: '綠喵黨',
+        partner: '喵清得 (綠喵黨)',
+        partisanIcon: greenIcon,
+        background: '政治界：大學教授、國際談判代表',
+        learningPortfolio: [
+            '國立臺灣大學法律系畢業（1978）',
+            '美國康乃爾大學法學碩士（1980）',
+        ],
+        experience: [
+            '國立政治大學、東吳大學教授',
+            '經濟部國際組織首席顧問（1993-2000）',
+            '經濟部貿易調查委員（1993-2000）',
+            '行政院陸委會諮委（1994-1998）',
+            '行政院公平會委員（1995-1998）',
+            '國安會諮委（1999-2000）',
+            '行政院陸委會主委（2000-2004）',
+            '總統府國策顧問（2004-2005）',
+            '立法委員（2004-2006）',
+            '行政院消保會主委（2006-2007）',
+            '行政院副院長（2006-2007）',
+            '民主進步黨主席（2008-2012、2014-2018）',
+            '中華民國總統（2016-）',
+        ],
+    },
+    {
+        candidate: 'guoYu',
+        chineseName: '汪國魚',
+        number: '3',
+        fullBodyPhoto: guoYu_fullBody,
+        headShot: guoYu_headShot,
+        partisan: '藍汪黨',
+        partner: '喵善政 (無黨籍)',
+        partisanIcon: blueIcon,
+        background: '政治界：民意代表',
+        learningPortfolio: [
+            '東吳大學英國語文學系畢業（1985）',
+            '國立政治大學東亞研究所法學碩士（1988）',
+        ],
+        experience: [
+            '世新專科講師',
+            '維多利亞雙語中小學創辦人',
+            '臺北縣議員（1990-1993）',
+            '立法委員（1993-2002）',
+            '臺北縣中和市副市長（2005-2006）',
+            '臺北農產運銷公司總經理（2013-2017）',
+            '中國國民黨高雄市黨部主委（2017-2019）',
+            '高雄市市長（2018-）',
+        ],
+    },
+];
+
+export default candidateData;
--- src/page/CandidateInfo/index.tsx
+++ src/page/CandidateInfo/index.tsx
@@ -1,28 +1,67 @@
 import './index.scss';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import candidateData from './candidateData';
 
 const CandidateInfo = () => {
     const { candidate } = useParams();
 
-    console.log('candidate', candidate);
+    const info = candidateData.filter(
+        (data) => data.candidate === candidate
+    )[0];
+
+    const otherCandidate = candidateData.filter(
+        (data) => data.candidate !== candidate
+    );
 
     return (
         <div className="candidateInfo">
-            <div>照片</div>
-            <div>1.喵楚魚</div>
-            <div>政黨</div>
-            <div>橘喵黨</div>
-            <div>競選搭檔</div>
-            <div>魚喵喵(無黨籍)</div>
-            <div>背景</div>
-            <div>政治界：公職、國際關西研究員</div>
-            <div>學歷</div>
-            <div>國立政治大學外交系畢業</div>
-            <div>經歷</div>
-            <div>行政院簡任秘書</div>
-            <div>看其他候選人</div>
-            <div>喵英文</div>
-            <div>汪國魚</div>
+            <h6>中華民國第十五任總統暨副總統選舉 / {info.chineseName}</h6>
+            <img
+                className="candidateInfo__photo"
+                src={info.fullBodyPhoto}
+                alt=""
+            />
+            <h3>
+                <div className="candidateInfo__number">{info.number}</div>
+                {info.chineseName}
+            </h3>
+            <h4>政黨</h4>
+            <div className="candidateInfo__content">
+                <img
+                    className="candidateInfo__icon"
+                    src={info.partisanIcon}
+                    alt=""
+                />
+                {info.partisan}
+            </div>
+            <h4>競選搭檔</h4>
+            <div className="candidateInfo__content">{info.partner}</div>
+            <h4>背景</h4>
+            <div className="candidateInfo__content">{info.background}</div>
+            <h4>學歷</h4>
+            <div className="candidateInfo__content">
+                {info.learningPortfolio.map((portfolio) => (
+                    <li>{portfolio}</li>
+                ))}
+            </div>
+            <h4>經歷</h4>
+            <div className="candidateInfo__content">
+                {info.experience.map((experience) => (
+                    <li>{experience}</li>
+                ))}
+            </div>
+            <h5>看其他候選人</h5>
+            <div className="candidateInfo__footer">
+                {otherCandidate.map((candidate) => (
+                    <Link
+                        className="candidateInfo__card"
+                        to={`/CandidateInfo/${candidate.candidate}`}
+                    >
+                        <img src={candidate.headShot} alt="" />
+                        <p>{candidate.chineseName}</p>
+                    </Link>
+                ))}
+            </div>
         </div>
     );
 };
